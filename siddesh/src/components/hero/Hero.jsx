b8b2c49b4a038106d47f74466b70f4e84c1b37b5
pramodsiddesh <pramodsiddesh144@gmail.com>
2023-02-23 15:23:01 +0530

    first commit

---
+++ siddesh/src/components/hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+const Hero = () => {
+  return (
+    <div className='parent__div'>
+        <div className='content__section'>
+            <h1>world</h1>
+        </div>
+        <div className='logo__section'>
+         <h1>hello</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ siddesh/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <h1>hiiiiiiiiiiii</h1>
+    </div>
+  )
+}
+
+export default Navbar
